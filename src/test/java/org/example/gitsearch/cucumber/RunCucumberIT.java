8a4a66a14a16a01c6755d1f084c645e7ef9b69c8
Sibonelo <sibonelo@aluvesqe.co.za>
2023-07-31 16:48:57 +0200

    Final work

--- src/test/java/org/example/gitsearch/cucumber/RunCucumberIT.java
+++ src/test/java/org/example/gitsearch/cucumber/RunCucumberIT.java
@@ -8,6 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources",
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "json:target/cucumber-reports/cucumber.json"},
         tags="@gitsearch"
 )
 
